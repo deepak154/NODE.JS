a9de82c0e660a39d781ae0edfc5236266aba9a67
deepak154 <deepakbhandari154@gmail.com>
2019-08-20 00:03:47 +0530

    For common layout in a pug template

---
+++ Pug With Layout File/Routes/admin.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const path = require('path');
+const router = express.Router();
+const rootDir = require('../util/path.js');
+const products = [];
+
+router.get('/add-product',(req,res, next)=>{
+	console.log("In another middleware1");
+	res.render('add-product',{pageTitle:'Add Product Page', path: '/admin/add-product'});
+}); 
+
+router.post('/add-product',(req,res, next)=>{ 
+	console.log(req.body);
+	products.push({title: req.body.title}); 
+	res.redirect('/');
+});
+
+exports.routes = router;
+exports.products = products;
